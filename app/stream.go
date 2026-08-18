6e268c7befba693ecd219c405c1ce95542ccd40a
2399matt <notoriousQR@gmail.com>
2026-08-18 16:53:18 -0400

    codecrafters submit [skip ci]

--- app/stream.go
+++ app/stream.go
@@ -81,6 +81,9 @@ func parseID(raw string) (IDRequest, error) {
 }
 
 func parseRangeID(raw string, isStart bool) (StreamID, error) {
+	if isStart && raw == "-" {
+		return StreamID{0, 0}, nil
+	}
 	parts := strings.Split(raw, "-")
 	ms, err := strconv.ParseInt(parts[0], 10, 64)
 	if err != nil {
