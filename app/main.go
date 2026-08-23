c9fb124bd4076b7af899d35424063f0c9040671b
2399matt <notoriousQR@gmail.com>
2026-08-22 23:01:03 -0400

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
 	"os"
@@ -9,15 +10,19 @@ import (
 var storage = NewStorage()
 
 func main() {
-	listener, err := net.Listen("tcp", "0.0.0.0:6379")
+	var port string
+	flag.StringVar(&port, "port", "6379", "port to run redis on")
+	flag.Parse()
+	listener, err := net.Listen("tcp", "0.0.0.0:"+port)
 	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
+		fmt.Printf("Failed to bind to port %s\n", port)
 		os.Exit(1)
 	}
+	fmt.Printf("Listening on port: %s\n", port)
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
-			fmt.Printf("unable to accept client: %v", err)
+			fmt.Printf("unable to accept client: %v\n", err)
 			continue
 		}
 		client := &Client{Conn: conn, queue: make([]Value, 0), watchQueue: make(map[string]struct{})}
